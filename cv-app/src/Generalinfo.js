da5afa4e10ff74933254d8d81e61e4725175e846
Luke Jennings <lukejennings123@gmail.com>
2022-09-13 00:45:27 +0100

    created a general info React dom addition

--- cv-app/src/Generalinfo.js
+++ cv-app/src/Generalinfo.js
@@ -1,7 +1,8 @@
 /* This section needs to have general info like: Name, Email, Phone Num, address. It needs to have a text box 
-for each input field. A button should be added to add the information*/
+for each input field. the info should be stored in an object.  A button should be added to add the information */
 
-import {useState} from 'react';
+import { render } from '@testing-library/react';
+import React, {useState, useEffect} from 'react';
 
 function GeneralInfo() {
 
@@ -12,17 +13,27 @@ function GeneralInfo() {
     const [num, setNum] = useState('');
     const [address, setaddress] = useState('');
 
+   const add =  useEffect(() => {
+        console.log('updated');
+    },[]);
 
-    console.log()
 
-    function Submit(e) {
-        e.preventDefault();
-        
-        console.log(e.altKey)
-
+    function Submit() {
+        const generalInfoObject = {
+            userFirstName: firstName, 
+            userLastName: lastName,
+            userTitle: title, 
+            userEmail: email,
+            userNumber: num, 
+            userAddress: address
+        }      
+        const idGrab = document.getElementById('parentWrapper');
+        const create = document.createElement('div')
+        idGrab.appendChild(create)
+        create.textContent = generalInfoObject.userFirstName;
     }
     return (
-        <div>
+        <div id = 'parentWrapper' className='parentWrapper'>
             {/* the setName hook will be called whenever a change to the event (e) is detected. This means that it will set 
             the 'name' variable in the setName hook to anything in the text box and update it everytime we type in a new letter into
             the textbox. Essentially it sets the 'name' variable to whatever is in the textbox as we type it in. 
@@ -36,11 +47,9 @@ function GeneralInfo() {
                 <input className = 'lastnameField' type='text' value={lastName} placeholder='Lastname' onChange={(e) => setLastName(e.target.value)}  required></input>
                 <input className = 'titleField' type='text' value={title} placeholder='Current Role Title' onChange={(e) => setTitle(e.target.value)}  required></input>
                 <input className = 'emailField' type='email' value={email} placeholder='Email' onChange={(e) => setEmail(e.target.value)}  required></input>
-                <input className = 'numField' type='number' value={num} placeholder='Contact Number' onChange={(e) => setNum(e.target.value)}  required></input>
+                <input className = 'numField' type='phone' value={num} placeholder='Contact Number' onChange={(e) => setNum(e.target.value)}  required></input>
                 <input className = 'addressField' type='text' value={address} placeholder='Address' onChange={(e) => setaddress(e.target.value)}  required></input>
-
                 <input type = 'button' onClick={Submit} value= 'button' ></input>
-
             </form>
         </div> 
     )
