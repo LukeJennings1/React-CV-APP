30357a0de61451180d57b4d3b28cccbdd6605d99
Luke Jennings <lukejennings123@gmail.com>
2022-09-16 19:24:28 +0100

    added functionality to submit more objects

--- cv-app/src/addJobExp.js
+++ cv-app/src/addJobExp.js
@@ -1,42 +1,49 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 function AddJobExp(){
 
 const [inputFields, setInputFields] = useState([
-    {position: '', company: '', location: '', from: '', to: '', id: 1},
-
+    {position: '', company: '', location: '', from: '', to: ''},
 ])
-const addInputBox = () => {
-    setInputFields([...inputFields,{position: '', company: '', location: '', from: '', to: '', id: 1}])
-    console.log(inputFields)
-} 
-const removeInputBox = (index) => {
-    const values = [...inputFields]
-    values.splice(index,1)
+const handleInputChange = (event, index) => {
+let value = [...inputFields]
+value[index][event.target.name] = event.target.value;
+setInputFields(value); 
 }
-//addInputBox takes the previous inputFields value and adds a new object to it. the spread operator (...) takes
-// the previous values which allows us to keep adding more objects to the array as it remembers the previously added
-// array objects.
-
+const addFields = () => {
+ let object = {position: '', company: '', location: '', from: '', to: ''}
+ setInputFields([...inputFields, object])
+}
+const submit = () => {
+    console.log(inputFields)
+}
     return (
         <div className='jobForm'>
-        <form className='jobFormSection'> {
-        inputFields.map((inputField, index) => (
-            <div key={index}>
-                <input type='text' placeholder='Position' value={inputField.position}/>
-                <div>{inputFields.position}</div>
-            </div>
-        )) }
-          </form>
-          <input type='button' value = 'Add' onClick = {addInputBox}></input>
-          <input type='button' value = 'Remove' onClick = {removeInputBox}></input>
+            <form>
+            {inputFields.map((form, index) => {
+                return (
+                    <div key={index}>
+                    <div>{form.position}</div>
+                    <input
+                    name='position'
+                    placeholder='position'
+                    onChange = {event => handleInputChange(event, index)}
+                    value = {form.position}
+
+                    />
+                </div>
+              )
+            })}
+        </form>
+
+        <button onClick={addFields}>add</button>
+        <button onClick={submit}>Submit</button>
+        <div className='tester'>{inputFields.position}</div>
 
         </div>
     );
 }
-//  Firstly, I want to take an array object and copy it on add button.
-
-
-// I want to take the input from a textbox and call a function when the submit button is pressed. 
+//  Firstly, I want to take an array object and copy it on add button - done.
+// I want to take the input from a textbox and submit it to the new object via onChange. 
 
 export default AddJobExp;
